fdb3fba11c6d197b91e9d40b88fe54fc50bc0a10
ybsdegit <ybsdeyx@163.com>
2019-01-19 22:21:29 +0800

    一些东西

---
+++ StudyPacticePython/Base_Python/验证码的识别/图形验证码的识别.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/1/19 0:02
+# @Author  : Paulson
+# @File    : 图形验证码的识别.py
+# @Software: PyCharm
+# @define  : function
+# http://my.cnki.net/elibregister/CheckCode.aspx
+import tesserocr
+from PIL import Image
--- StudyPacticePython/验证码/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/practice/Study_Test python
-# -*- coding: utf-8 -*-
-# @Time    : 2019/1/2 23:51
-# @Author  : yb.w
-# @File    : __init__.py.py 
